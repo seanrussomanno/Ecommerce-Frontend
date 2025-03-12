931479218697cfcd85803132e754783780183019
Sean Russomanno <bateman.sean@proton.me>
2025-03-12 15:01:33 -0400

    Added drop down menu and integrated the prices to change with the product, keeping one button.

--- frontend/product.js
+++ frontend/product.js
@@ -2,23 +2,43 @@ const classicPrice = 11.99;
 const cherryPrice = 17.99;
 const quantumPrice = 24.99;
 
-let classicInStock = 81;
-let cherryInStock = 28;
-let quantumInStock = 3;
-
 let cartTotal = 0;
-alert("hello")
-function addClassic() {
-    cartTotal += classicPrice;
-    alert("Classic added to cart! Cart total: $" + cartTotal.toFixed(2));
+let selectedProduct = "classic"; 
+
+
+function updatePrice() {
+    let priceDisplay = document.getElementById("selected-price");
+    
+    if (selectedProduct === "classic") {
+        priceDisplay.textContent = "$" + classicPrice.toFixed(2);
+    } else if (selectedProduct === "cherry") {
+        priceDisplay.textContent = "$" + cherryPrice.toFixed(2);
+    } else if (selectedProduct === "quantum") {
+        priceDisplay.textContent = "$" + quantumPrice.toFixed(2);
+    }
 }
 
-function addCherry() {
-    cartTotal += cherryPrice;
-    alert("Cherry added to cart! Cart total: $" + cartTotal.toFixed(2));
+
+function selectProduct(product) {
+    selectedProduct = product;
+    updatePrice();
 }
 
-function addQuantum() {
-    cartTotal += quantumPrice;
-    alert("Quantum added to cart! Cart total: $" + cartTotal.toFixed(2))
+
+function addToCart() {
+    if (selectedProduct === "classic") {
+        cartTotal += classicPrice;
+        alert("Classic added to cart! Cart total: $" + cartTotal.toFixed(2));
+    } else if (selectedProduct === "cherry") {
+        cartTotal += cherryPrice;
+        alert("Cherry added to cart! Cart total: $" + cartTotal.toFixed(2));
+    } else if (selectedProduct === "quantum") {
+        cartTotal += quantumPrice;
+        alert("Quantum added to cart! Cart total: $" + cartTotal.toFixed(2));
+    }
 }
+
+
+window.onload = function() {
+    updatePrice(); 
+};
